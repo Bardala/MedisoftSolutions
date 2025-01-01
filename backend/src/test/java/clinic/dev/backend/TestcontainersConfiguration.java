23acdda717d0afb769b67dbff8b1938d624e360f
bardala <islambardala@gmail.com>
2025-01-01 23:36:39 +0200

    init backend with springboot

---
+++ backend/src/test/java/clinic/dev/backend/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package clinic.dev.backend;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
