ff22fa100c730f38110004099fc871f9c10dda47
bardala <islambardala@gmail.com>
2025-01-07 17:26:43 +0200

    Using lombok/

--- backend/src/main/java/clinic/dev/backend/dto/SigninResponse.java
+++ backend/src/main/java/clinic/dev/backend/dto/SigninResponse.java
@@ -1,28 +1,13 @@
 package clinic.dev.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SigninResponse {
   private String token;
   private String username;
-
-  public SigninResponse(String token, String username) {
-    this.token = token;
-    this.username = username;
-  }
-
-  public String getToken() {
-    return token;
-  }
-
-  public void setToken(String token) {
-    this.token = token;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
 }
